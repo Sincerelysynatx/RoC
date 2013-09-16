5f6d086fa77874f58b9e66d32ef432f59b3b3e7b
Sincerelysynatx <sean_pimentel@hotmail.com>
2013-09-16 11:25:08 -0700

    First Commit

---
+++ src/com/sincerelysmarty/RoC/RoC.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sincerelysmarty.RoC;
+
+import com.sincerelysmarty.RoC.States.MainMenu;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author Sean & David
+ */
+public class RoC extends StateBasedGame {
+
+    private static final String gameName = "Realm of Corruption";
+    public static final int menu = 0;//Main menu when launching game
+    public static final int settings = 1;//Options for graphics
+    public static final int play = 2;//Actual play state
+    public static final int hero = 3;//Menu for viewing player stats
+    public static final int abilities = 4;//Menu for selecting abilities
+    public static final int skills = 5;
+//    public static final int play = 1;
+//    public static final int play = 1;
+    
+    
+    public RoC(String gameName){
+        super(gameName);
+        this.addState(new MainMenu(menu));
+        //this.addState(new Settings(settings));
+        //this.addState(new Play(play));
+        //this.addState(new Hero(hero));
+//        this.addState(new Play(play));
+//        this.addState(new Play(play));
+//        this.addState(new Play(play));
+//        this.addState(new Play(play));
+//        this.addState(new Play(play));
+//        this.addState(new Play(play));
+    }
+    
+    public static void main(String[] args){
+        AppGameContainer appgc;
+        try {
+            appgc = new AppGameContainer(new RoC(gameName));
+            appgc.setDisplayMode(appgc.getScreenWidth(), appgc.getScreenHeight(), false);
+            appgc.setFullscreen(true);
+            appgc.start();
+        } catch (SlickException e) {
+        }
+    }
+    
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.getState(menu).init(gc, this);
+        this.enterState(menu);
+    }
+}
---
+++ src/com/sincerelysmarty/RoC/States/MainMenu.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sincerelysmarty.RoC.States;
+
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author Sean & David
+ */
+public class MainMenu extends BasicGameState{
+
+    public MainMenu(int menu) {
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        
+    }
+}
