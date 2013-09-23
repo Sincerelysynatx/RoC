5c87d206d9d6d3cc8f325664ada86eb0f70329b5
5002394184 <5002394184@HNPOD02-06.Student.CSN.EDU>
2013-09-23 12:15:36 -0700

    Minor changes to About menu, started on a background for text

--- src/com/sincerelysmarty/RoC/Client/Output/ParagraphOutput.java
+++ src/com/sincerelysmarty/RoC/Client/Output/ParagraphOutput.java
@@ -5,6 +5,7 @@
 package com.sincerelysmarty.RoC.Client.Output;
 
 import com.sincerelysmarty.RoC.Client.Font;
+import com.sincerelysmarty.RoC.Client.Image;
 import com.sincerelysmarty.RoC.Client.RoC;
 import com.sincerelysmarty.RoC.Client.Screen;
 import com.sincerelysmarty.RoC.Client.States.MenuComponent;
@@ -19,9 +20,12 @@ public class ParagraphOutput extends MenuComponent {
     private String[] lines;
     private char nextChar;
     private int index;
+    private static Image txtBckgrnd = new Image("res/textBackground.png");
+    private boolean doesHaveBackground;
 
-    public ParagraphOutput(String text, int number) {
+    public ParagraphOutput(String text, int number, boolean doesHaveBackground) {
         this.text = text;
+        this.doesHaveBackground = doesHaveBackground;
         lines = new String[number];
         index = 0;
         nextChar = text.charAt(index);
@@ -69,9 +73,41 @@ public class ParagraphOutput extends MenuComponent {
 
     @Override
     public void render(Screen screen) {
-        super.render(screen); //To change body of generated methods, choose Tools | Templates.
         for (int i = 0; i < lines.length; i++) {
+            /*
+            if (doesHaveBackground) {
+                for (int j = 10; j < RoC.GAME_WIDTH - 10; j += 7) {
+                    
+                }
+            }
+            * */
             Font.draw(lines[i], screen, 10, 20 * i + 100);
+        }
+    }
+
+    private void drawTextBackground(int bckgrndImg, Screen screen) {
+        switch (bckgrndImg) {
+            case 0:
+                break;
+            case 1:
+                screen.draw(txtBckgrnd.subImage[0][0], index, index);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
         }
     }
 }
